4d60e7472d2d5eb37721005ec05846b0b1dd0cf0
ShamimaNasrin <shamima.nasrion007@gmail.com>
2022-11-22 14:36:26 +0600

    jwt one

--- index.js
+++ index.js
@@ -76,6 +76,16 @@ async function run() {
             res.send(result);
         });
 
+        //create jwt token
+        app.get('/jwt', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            console.log(user);
+            res.send({ accessToken: 'token' })
+        });
+
+        //sent user info to mongodb
         app.post('/users', async (req, res) => {
             const user = req.body;
             console.log(user);
